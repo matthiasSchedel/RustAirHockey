932d7b2280e84c3354b6c494a8879c4e37558658
ugemg <ugemg@student.kit.edu>
2019-04-03 15:52:51 +0200

    add gdb.bat and config (outcomment windows runner!)

--- gdb.bat
+++ gdb.bat
@@ -1,3 +1,3 @@
 echo "Please run 'st-util' in another terminal window"
 echo ""
-arm-none-eabi-gdb -iex "add-auto-load-safe-path ." -ex "tar ext :4242" -ex "load-reset" %1
+arm-none-eabi-gdb -iex "add-auto-load-safe-path ." -ex "tar ext :3333" -ex "load-reset" %1
