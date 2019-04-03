20b979d0d214197f4f11648679d2993460da3ddc
ugemg <ugemg@student.kit.edu>
2019-04-03 14:55:20 +0200

    create game, field and player classes

---
+++ src/airhockey/field.rs
@@ -0,0 +1,5 @@
+//! Airhockey game field.
+
+pub const WIDTH_MAX : u16 = 523;
+pub const HEIGHT_MAX : u16 = 293;
+
--- src/airhockey/game.rs
+++ src/airhockey/game.rs
@@ -1,1 +1,36 @@
 //! Airhockey game.
+
+extern crate alloc;
+
+
+use super::field;
+use super::player::Player;
+use super::player;
+use alloc::vec::Vec;
+use stm32f7_discovery::touch;
+
+pub struct Game {
+    players: Vec<Player>
+}
+impl Game {
+  pub fn new(number_players : u8) -> Game{ 
+      let players = Vec::new();
+      for p in 0..number_players {
+         players.push(Player::new(p)) 
+      }
+
+      Game {
+          players: players,
+      }
+  }  
+  pub fn is_touched(&self, p_id : usize) -> bool{
+    self.players[p_id].get_position();
+    for touch in &touch::touches(&mut i2c_3).unwrap() {
+        touch.x
+        return false;
+    }
+  }
+}
+
+
+
