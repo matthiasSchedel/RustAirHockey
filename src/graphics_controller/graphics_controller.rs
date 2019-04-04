40da5d4526b6ff78e1eab555a33a183a47e9611a
Matthias Schedel <matthias.schedel@student.kit.edu>
2019-04-04 15:45:56 +0200

    update physics and ghraphics controller

--- src/graphics_controller/graphics_controller.rs
+++ src/graphics_controller/graphics_controller.rs
@@ -1,9 +1,9 @@
 //! Graphics controller.
-const STROKE_COLOR:u16 = 0xffff00;
-const USE_STROKE:bool = true;
-const PLAYER_SIZE:u16 = 10;
-const PUCK_SIZE:u16 = 6;
-const BACKGROUND_COLOR:u16 = 0xfff000;
+const STROKE_COLOR: u32 = 0xffff00;
+const USE_STROKE: bool = true;
+const PLAYER_SIZE: u16 = 10;
+const PUCK_SIZE: u16 = 6;
+const BACKGROUND_COLOR: u32 = 0xfff000;
 // GraphicsController struct
 pub struct GraphicsController {
     display: i32,
@@ -11,8 +11,6 @@ pub struct GraphicsController {
     width: u16,
     //display height
     height: u16,
-    //background color used in controller
-    background_color: u16,
 }
 impl GraphicsController {
     // game constructor
@@ -20,7 +18,7 @@ impl GraphicsController {
         GraphicsController {
             display: 2,
             width: width,
-            height: height
+            height: height,
         }
     }
     // is touched method
@@ -33,10 +31,10 @@ impl GraphicsController {
         &self,
         color: u16,
         pos_x: u16,
-        pos_y: u16,
-        radius: f32,
-        draw_stroke: bool,
-        stroke_color: u16,
+        // pos_y: u16,
+        // radius: f32,
+        // draw_stroke: bool,
+        // stroke_color: u16,
     ) {
 
     }
--- src/physics_controller/physics_controller.rs
+++ src/physics_controller/physics_controller.rs
@@ -1,6 +1,9 @@
 //! Graphics controller.
-const FRICTION:u16 = 0xffff00;
-const USE_FRICTION:bool = true;
+use crate::alloc;
+use alloc::vec::Vec;
+
+const FRICTION: u32 = 0xffff00;
+const USE_FRICTION: bool = true;
 
 // PhysicsController struct
 pub struct PhysicsController {
@@ -9,49 +12,61 @@ pub struct PhysicsController {
     width: u16,
     //display height
     height: u16,
-    ball_pos: Vec<u16>
+    ball_pos: Vec<u16>,
 }
 
-struct CollisionObject {
-    hasCollided:bool,
-    collision_pos:Vec<u16>
+pub struct CollisionObject {
+    has_collided: bool,
+    collision_pos: Vec<u16>,
 }
+
+impl CollisionObject {
+    // game constructor
+    pub fn new(has_collided: bool, pos_x: u16, pos_y: u16) -> CollisionObject {
+        CollisionObject {
+            has_collided: has_collided,
+            collision_pos: vec![pos_x, pos_y],
+        }
+    }
+}
+
 impl PhysicsController {
     // game constructor
     pub fn new(width: u16, height: u16) -> PhysicsController {
         PhysicsController {
             display: 2,
             width: width,
-            height: height
+            height: height,
+            ball_pos: vec![0, 0],
         }
     }
 
-    pub fn set_ball_pos(&self, pos_x:u16, pos_y:u16) {
-        self.ball_pos = Vec![pos_x, pos_y];
+    pub fn set_ball_pos(&self, pos_x: u16, pos_y: u16) {
+        // self.ball_pos = vec![pos_x, pos_y];
     }
 
-    pub fn update_ball_position(&self, old_x:u16, old_y:u16, radius:u16, coll:CollisionObject) {
-        let border_collisions:(bool, Vec<u16>) = calculate_border_collision_point(old_x:u16, old_y:u16, radius:u16);
-        if border_collisions.0 {
-            update_pos_from_coll_point(border_collisions.1);
+    pub fn update_ball_position(&self, old_x: u16, old_y: u16, radius: u16, coll: CollisionObject) {
+        let border_collisions: CollisionObject =
+            self.calculate_border_collision_point(old_x, old_y, radius);
+        if border_collisions.has_collided {
+            self.update_pos_from_coll_point(border_collisions);
         } else {
-        update_ball_pos_without_coll();
+            self.update_ball_pos_without_coll();
         }
     }
-    fn update_pos_from_coll_point(&self, coll: CollisionObject) {
+    fn update_pos_from_coll_point(&self, coll: CollisionObject) {}
 
-    }
+    fn update_ball_pos_without_coll(&self) {}
 
-    fn update_ball_pos_without_coll(&self) {
-
-    }
-
-    fn calculate_border_collision_point(&self, old_x:u16, old_y:u16, radius:u16) -> CollisionObject {
-        return CollisionObject { false , Vec![true, Vec![old_x,old_y]]};
+    fn calculate_border_collision_point(
+        &self,
+        old_x: u16,
+        old_y: u16,
+        radius: u16,
+    ) -> CollisionObject {
+        return CollisionObject::new(false, old_x, old_y);
     }
 
     //input some circle object
-    pub fn calculate_circle_coll_with_ball(&self) {
-
-    }
+    pub fn calculate_circle_coll_with_ball(&self) {}
 }
