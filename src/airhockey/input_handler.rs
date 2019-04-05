097039acfbd98efd99b72728852ed91248ae1e9e
Matthias Schedel <matthias.schedel@student.kit.edu>
2019-04-05 15:04:11 +0200

    add inputhandler

---
+++ src/airhockey/input_handler.rs
@@ -0,0 +1,24 @@
+use super::{ball::self, ball::Ball};
+use crate::input::input::Input;
+
+const DRAW_LAYER_NO: u8 = 1;
+
+pub struct InputHandler<'a> {
+  input: &'a Input<'static>,
+  screen_size:[u16;2]
+}
+
+impl<'a> InputHandler<'a> {
+    pub fn new(input: &'a Input<'static>) -> InputHandler<'a> {
+      InputHandler { 
+        input:Input,
+        screen_size:[480,272]
+      }
+  } 
+  // Ball constructor
+
+  pub fn calculate_new_pos_from_input(pos: [u16;2]) -> (bool, [u16;2])  {
+    return {(false,[0,0])};
+  }
+  
+}
