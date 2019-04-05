3128a05244c7e7db4a38e49a2de9ea548cfab2a5
Matthias Schedel <matthias.schedel@student.kit.edu>
2019-04-05 18:40:52 +0200

    optimize display class

--- examples/display.rs
+++ examples/display.rs
@@ -12,15 +12,12 @@ use alloc_cortex_m::CortexMHeap;
 use core::alloc::Layout as AllocLayout;
 use core::panic::PanicInfo;
 use rt::{entry, exception};
-use stm32f7::stm32f7x6::{Peripherals};
+use stm32f7::stm32f7x6::Peripherals;
 use stm32f7_discovery::{
     gpio::{GpioPort, OutputPin},
     init,
-    system_clock::{self},
-    lcd::{self, Color,FramebufferArgb8888},
-    touch,
-    
-    
+    lcd::{self, Color, FramebufferArgb8888},
+    system_clock, touch,
 };
 
 #[entry]
@@ -31,7 +28,6 @@ fn main() -> ! {
     let mut pwr = peripherals.PWR;
     let mut ltdc = peripherals.LTDC;
     let mut fmc = peripherals.FMC;
-
 
     init::init_system_clock_216mhz(&mut rcc, &mut pwr, &mut flash);
     init::enable_gpio_ports(&mut rcc);
@@ -50,15 +46,15 @@ fn main() -> ! {
     let mut pins = init::pins(
         gpio_a, gpio_b, gpio_c, gpio_d, gpio_e, gpio_f, gpio_g, gpio_h, gpio_i, gpio_j, gpio_k,
     );
-    
+
     //fuers Display brauchen wir den sdram und das lcd
     init::init_sdram(&mut rcc, &mut fmc);
     let mut lcd = lcd::init(&mut ltdc, &mut rcc);
-    
+
     // das display hat 2 Layer auf die wir zeichnen können
     let mut layer1 = lcd::Lcd::layer_1(&mut lcd).unwrap();
     let mut layer2 = lcd::Lcd::layer_2(&mut lcd).unwrap();
-    
+
     // dass wir was sehen, machen wir das Display und das Licht an
     pins.display_enable.set(true);
     pins.backlight.set(true);
@@ -66,21 +62,19 @@ fn main() -> ! {
     // alte Sachen wegraeumen
     layer1.clear();
     layer2.clear();
-    
 
     let mut i2c_3 = init::init_i2c_3(peripherals.I2C3, &mut rcc);
     i2c_3.test_1();
     i2c_3.test_2();
-   
+
     // poll for new touch data
-   
-    
+
     loop {
         //und jetzt können wir alles machen, was wir auf Layern machen können.
-        let length=20;
-        let hight=10;
-        let HIGHT=272;
-        let LENGTH=480;
+        let length = 20;
+        let hight = 10;
+        let HIGHT = 272;
+        let LENGTH = 480;
         let mut max_length;
         let mut max_hight;
         let mut old_touchx: usize;
@@ -88,22 +82,25 @@ fn main() -> ! {
 
         for touch in &touch::touches(&mut i2c_3).unwrap() {
             // Check if rectangle is bigger then screen
-            if touch.x as usize+length >LENGTH{
-                max_length=LENGTH;
-            }
-            else{
-                max_length=touch.x as usize+length;
+            if touch.x as usize + length > LENGTH {
+                max_length = LENGTH;
+            } else {
+                max_length = touch.x as usize + length;
             }
 
-            if touch.y as usize + hight>HIGHT{
-                max_hight=HIGHT;
+            if touch.y as usize + hight > HIGHT {
+                max_hight = HIGHT;
+            } else {
+                max_hight = touch.y as usize + hight;
             }
-            else{
-                max_hight= touch.y as usize + hight;
-            }
-        //draw_rectangle(&mut layer1, touch.x as usize, touch.y as usize, max_length, max_hight, Color::from_hex(0xff0_000))
-        draw_circle(&mut layer1, touch.x as usize, touch.y as usize, 20, Color::from_hex(0xff0_000))
-       
+            //draw_rectangle(&mut layer1, touch.x as usize, touch.y as usize, max_length, max_hight, Color::from_hex(0xff0_000))
+            draw_circle(
+                &mut layer1,
+                touch.x as usize,
+                touch.y as usize,
+                20,
+                Color::from_hex(0xff0_000),
+            )
         }
     }
 }
@@ -149,8 +146,8 @@ pub fn draw_circle(
 ) {
     for y in y_centre - radius..=y_centre + radius {
         for x in x_centre - radius..=x_centre + radius {
-            if x * x + x_centre * x_centre - 2 * x * x_centre + y * y
-                + y_centre * y_centre - 2 * y * y_centre
+            if x * x + x_centre * x_centre - 2 * x * x_centre + y * y + y_centre * y_centre
+                - 2 * y * y_centre
                 <= radius * radius
             {
                 layer.print_point_color_at(x as usize, y as usize, color);
@@ -158,7 +155,6 @@ pub fn draw_circle(
         }
     }
 }
-
 
 pub fn draw_rectangle(
     layer: &mut lcd::Layer<FramebufferArgb8888>,
@@ -169,11 +165,8 @@ pub fn draw_rectangle(
     color: lcd::Color,
 ) {
     for x in x_start..=length {
-        for y in y_start ..=hight{
-                layer.print_point_color_at(x as usize, y as usize, color);
-            }
+        for y in y_start..=hight {
+            layer.print_point_color_at(x as usize, y as usize, color);
+        }
     }
 }
-
-
-
