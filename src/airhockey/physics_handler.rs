f64926421176673fd8d59950e4b57cb4530083e7
Matthias Schedel <matthias.schedel@student.kit.edu>
2019-04-05 14:58:28 +0200

    add physics handler

---
+++ src/airhockey/physics_handler.rs
@@ -0,0 +1,22 @@
+use crate::physics::physics::Physics;
+
+pub struct PhysicsHandler {
+  physics: Physics,
+  screen_size:[u16;2]
+}
+
+impl PhysicsHandler {
+    pub fn new(physics: Physics) -> PhysicsHandler {
+      PhysicsHandler { 
+        physics:physics,
+        screen_size:[480,272]
+      }
+  } 
+  // Ball constructor
+
+ // (true,_) if collision, (,_) and new speed after collection
+  pub fn calculate_border_coll(pos: [u16;2]) -> (bool, [u16;2])  {
+    return {(false,[0,0])};
+  }
+  
+}
