6ad5f6e00f6663dae5b166f1a099835cf1ff3ef5
Matthias Schedel <matthias.schedel@student.kit.edu>
2019-04-05 14:58:50 +0200

    change touch controller -> input

---
+++ src/input/input.rs
@@ -0,0 +1,34 @@
+//! Touch controller.
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+pub struct Input {
+    touch: i32,
+    // display width
+    width: u16,
+    //display height
+    height: u16,
+}
+impl Input {
+    // game constructor
+    pub fn new(width: u16, height: u16) -> Input {
+        Input {
+            touch: 2,
+            width: width,
+            height: height,
+        }
+    }
+    // is touched method
+    pub fn is_touched(&self, p_id: usize) -> bool {
+        return false;
+    }
+
+    pub fn init(&self) {}
+
+    pub fn get_touch_positions(&self) -> (alloc::vec::Vec<u16>, alloc::vec::Vec<u16>) {
+        let mut positions_x: Vec<u16> = Vec::new();
+        let mut positions_y: Vec<u16> = Vec::new();
+        (positions_x, positions_y)
+    }
+}
