18808b973f6eb0e34b26dbdb5dd06e65f321cd91
Matthias Schedel <matthias.schedel@student.kit.edu>
2019-04-05 11:45:58 +0200

    fix lifetimes for graphics_handler und graphics_controller

--- src/airhockey/graphics_handler.rs
+++ src/airhockey/graphics_handler.rs
@@ -3,18 +3,26 @@ use crate::graphics_controller::graphics_controller::GraphicsController;
 
 const DRAW_LAYER_NO: u8 = 1;
 
-pub struct GraphicsHandler {
-  graphics_controller: GraphicsController
+pub struct GraphicsHandler<'a> {
+  graphics_controller: &'a GraphicsController<'static>,
+  screen_size:[u16;2]
 }
 
-impl GraphicsHandler {
-    pub fn new(graphics_controller: GraphicsController) {
-
+impl<'a> GraphicsHandler<'a> {
+    pub fn new(graphics_controller: &'a GraphicsController<'static>) -> GraphicsHandler<'a> {
+      GraphicsHandler { 
+        graphics_controller:graphics_controller,
+        screen_size:[480,272]
+      }
   } 
   // Ball constructor
 
   pub fn draw_ball(&self, ball_: Ball) {
       self.graphics_controller.draw_circle(ball::COLOR, ball_.position, ball::RADIUS, ball::HAS_STROKE,  ball::STROKE_COLOR);
+  }
+
+  pub fn draw_playing_field(&self) {
+      
   }
   
 }
