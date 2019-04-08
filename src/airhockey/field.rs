4bd23f6d3fd3cb7ad7462942eb9f575789b91508
Marius Neugschwender <marius.neugschwender@live.de>
2019-04-08 15:54:48 +0200

    Added goal size

--- src/airhockey/field.rs
+++ src/airhockey/field.rs
@@ -2,6 +2,7 @@
 
 pub const WIDTH_MAX : u16 = 523;
 pub const HEIGHT_MAX : u16 = 293;
-pub const BACKGROUND_COLOR: u16 = 0x000fff;
+pub const BACKGROUND_COLOR: u32 = 0x000fff;
 pub const BORDER_WIDTH: u16=10;
+pub const GOAL_SIZE:u16 =50;
 
