94c606e73123083fc154b1be164693ace10bfa4f
Marius Neugschwender <marius.neugschwender@live.de>
2019-04-08 17:04:54 +0200

    Added player to other thing

--- src/airhockey/player.rs
+++ src/airhockey/player.rs
@@ -3,14 +3,16 @@
 use super::field;
 
 // General player properties
-pub const PLAYER_RADIUS: u16 = 10;
-pub const PLAYER_COLOR: u8 = 150;
+pub const RADIUS: u16 = 10;
+pub const COLOR: u32 = 150;
+pub const STROKE_COLOR: u32 = 0xfff000; 
+pub const HAS_STROKE: bool = true; 
+
 
 // Player
 pub struct Player {
     pub player_id: u8,
-    pub xpos_centre: u16,
-    pub ypos_centre: u16,
+    pub position: [u16;2],
 }
 
 impl Player {
@@ -21,20 +23,18 @@ impl Player {
         if player_id == 0 {
             Player {
                 player_id: player_id,
-                xpos_centre : field::WIDTH_MAX/4,
-                ypos_centre : field::HEIGHT_MAX/2,   
+                position : [field::WIDTH_MAX/4,field::HEIGHT_MAX/2],   
             }
         }
         else{
             Player {
                 player_id : player_id,
-                xpos_centre : 3* field::WIDTH_MAX/4,
-                ypos_centre : field::HEIGHT_MAX,
+                position : [3* field::WIDTH_MAX,field::HEIGHT_MAX],
             }
         }
     }
     
     pub fn get_position(&self) -> (u16, u16) {
-        return (self.xpos_centre, self.ypos_centre);
+        return (self.position[0], self.position[1]);
     }
 }
